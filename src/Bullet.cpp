76f33437f706b3eefb43814c267b606b19279061
enricogeier <enrico.geier130900@gmail.com>
2024-02-28 12:44:36 +0100

    shoot 4

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -4,7 +4,7 @@ void Bullet::move(float &delta)
 {
     if(state == State::NORMAL)
     {
-        position += direction * (float)speed * delta;
+        position += direction.clamp() * (float)speed * delta;
 
         collision_circle.origin = collision_circle.initial_origin + position;
     }
